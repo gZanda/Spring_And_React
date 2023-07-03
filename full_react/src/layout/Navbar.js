a957c6f1aef168be559c73f7412c3abc5dd5293a
gZanda <gabrielfzanda@gmail.com>
2023-07-03 15:18:54 -0300

    Navbar and Table

--- full_react/src/layout/Navbar.js
+++ full_react/src/layout/Navbar.js
@@ -6,7 +6,15 @@ export default function Navbar() {
 
         <div>
 
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Full Stack App</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <button className="btn"> Add User </button>
+            </nav>
+
         </div>
-        
+
     )
 }
