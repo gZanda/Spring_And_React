52516ed0e0738a3c64d7c00e4eef5687f193f417
gZanda <gabrielfzanda@gmail.com>
2023-07-03 20:36:08 -0300

    COMPLETE

--- full_react/src/pages/Home.js
+++ full_react/src/pages/Home.js
@@ -59,7 +59,6 @@ export default function Home() {
                                 <td>{user.username}</td>
                                 <td>{user.email}</td>
                                 <td>
-                                    <button className="btn btn-primary mx-2" > View </button>
                                     <Link className="btn btn-outline-primary mx-2" to={`/EditUser/${user.id}`}> Edit </Link>
                                     <button className="btn btn-danger mx-2" onClick={()=>deleteUser(user.id)} > Delete </button>
                                 </td>
