19f998e1f8c60b8078844c916281cfd5abd8a59c
gZanda <gabrielfzanda@gmail.com>
2023-07-03 22:12:05 -0300

    Nav Link Adjust

--- full_react/src/layout/Navbar.js
+++ full_react/src/layout/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
         <div>
 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to={"/"} className="navbar-brand mx-3 " href="#">Full Stack App</Link>
+                <Link to="/" className="navbar-brand mx-3 " href="#">Full Stack App</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
